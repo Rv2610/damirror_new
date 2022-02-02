ae6335c9716cf20ca0c092ae930b0fe5f9bbba3f
rohit <vrrathod026@gmail.com>
2022-02-02 18:07:59 +0530

    added names of songs using pthon

--- home/views.py
+++ home/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from home.models import Songs
 
 def home(request):
-    return render(request,'index.html')
+    song = Songs()
+    song.name1 = "Kya schene"
+    song.name2 = "mauka e vardat"
+    song.name3 = "kya bolte"
+    song.name4 = "iya bhai"
+    params={'name1':song.name1,'name2':song.name2,'name3':song.name3,'name4':song.name4}
+    return render(request,'index.html',params)
+
+
 
